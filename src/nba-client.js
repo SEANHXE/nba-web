f32c18c66f26b4b869eee88b777f6b64f01fcf78
SEANHXE <790438599@qq.com>
2020-02-18 01:39:24 -0600

    1.3 Update Profile

--- src/nba-client.js
+++ src/nba-client.js
@@ -1,7 +1,7 @@
 import nba from 'nba';
 
-const SERVER_URL = 'https://34.67.117.84';
-//const SERVER_URL = 'https://35.235.84.235:5000';
+//const SERVER_URL = 'https://34.67.117.84';
+const SERVER_URL = 'http://35.235.84.235:5000';
 export default {
   ...nba,
   stats: {
