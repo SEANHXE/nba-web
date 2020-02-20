cdc329d9fbad9c96d8bdba397dd1cecd10f2b950
SEANHXE <790438599@qq.com>
2020-02-19 22:39:24 -0600

    2.2 CounterSlider component onConutSliderChange is callBack func

---
+++ src/components/CounterSlider.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import { Slider, InputNumber, Row, Col } from 'antd';
+
+class CounterSlider extends Component {
+
+  state = {
+    inputValue: 1,
+  };
+
+  onChange = value => {
+    const num = Number(value) ? value : this.state.inputValue;
+    this.setState({
+      inputValue: num,
+    });
+    this.props.onConutSliderChange(num);
+  };
+
+  render() {
+    const { inputValue } = this.state;
+    return (
+        <Row>
+          <Col span={12}>
+            <Slider
+                min={1}
+                max={20}
+                onChange={this.onChange}
+                value={inputValue}
+            />
+          </Col>
+          <Col span={4}>
+            <InputNumber
+                min={1}
+                max={20}
+                style={{ marginLeft: 16 }}
+                value={inputValue}
+                onChange={this.onChange}
+            />
+          </Col>
+        </Row>
+    );
+  }
+}
+
+export default CounterSlider;
--- src/components/DataViewContainer.js
+++ src/components/DataViewContainer.js
@@ -1,11 +1,28 @@
 import React, {Component} from 'react';
 import ShotChart from "./ShotChart"
+import CounterSlider from "./CounterSlider"
 
 class DataViewContainer extends Component {
+  state = {
+    minCount: 2
+  }
+
+  onCountSliderChange = (data) => {
+    console.log(data);
+  }
+
   render() {
     return (
       <div className="data-view">
-        <ShotChart playerId={this.props.playerId}/>
+        <ShotChart
+          playerId={this.props.playerId}
+          minCount={this.state.minCount}
+        />
+        <div className="filter">
+          <CounterSlider
+            onConutSliderChange={this.onCountSliderChange}
+          />
+        </div>
       </div>
     );
   }
